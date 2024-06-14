06fb9847035c7bb9cf17cb5d3ba3afcc9341d3fb
Artem_Lakatun <alakatuni@mail.ru>
2024-06-15 00:17:04 +0300

    feat: charList and char info are ready

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -35,7 +35,6 @@ class CharList extends Component {
     }
 
     // Этот метод создан для оптимизации,
-    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
         const items =  arr.map((item) => {
             let imgStyle = {'objectFit' : 'cover'};
@@ -53,7 +52,7 @@ class CharList extends Component {
                 </li>
             )
         });
-        // А эта конструкция вынесена для центровки спиннера/ошибки
+        // центровка спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
